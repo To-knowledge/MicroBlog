c6b1fce771cbedcc1acb26c690abb7e76de005c8
balancex <yuquanxiao@outlook.com>
2020-04-27 13:06:56 +0800

    Maven控制

---
+++ src/test/java/com/xiao/blogutimate/BlogutimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiao.blogutimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogutimateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
